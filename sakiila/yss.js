9b1fe19d40a1182f3b3964e6afba78230b08683b
Bob Bao <me@baobo.me>
2022-06-30 17:59:18 +0800

    update

--- sakiila/yss.js
+++ sakiila/yss.js
@@ -1,11 +1,8 @@
 var obj = JSON.parse($response.body);
 
-let key = '"显示时间":"2022-07-01 04:36"'
+var timeNow = new Date();
+var timeNowStr = timeNow.getFullYear() + "-" + (timeNow.getMonth() + 1) + "-" + timeNow.getDate() + " 03:27";
 
-// if (obj.data.match(/"显示时间":"(\d{4}-\d{2}-\d{2} \d{2}:\d{2})"/)) {
-//     obj.data = key;
-// }
-
-obj.data.records.显示时间 = "2022-07-01 07:01";
+obj.data.records[0].显示时间 = timeNowStr;
 
 $done({body: JSON.stringify(obj)});
