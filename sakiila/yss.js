c116c4159e24132bb93eed3ec4eba992aa37c32e
Bob Bao <me@baobo.me>
2022-07-01 09:47:33 +0800

    update

--- sakiila/yss.js
+++ sakiila/yss.js
@@ -1,8 +1,9 @@
-var obj = JSON.parse($response.body);
+let obj = JSON.parse($response.body);
 
-var timeNow = new Date();
-var timeNowStr = timeNow.getFullYear() + "-" + (("0" + (timeNow.getMonth() + 1)).slice(-2)) + "-" + timeNow.getDate() + " 03:27";
+let timeNow = new Date();
+const dateNow = timeNow.getFullYear() + "-" + (("0" + (timeNow.getMonth() + 1)).slice(-2)) + "-" + (("0" + timeNow.getDate()).slice(-2));
 
-obj.data.records[0].显示时间 = timeNowStr;
+obj.data.records[0].检测日期 = dateNow + " 03:27:56";
+obj.data.records[0].显示时间 = dateNow + " 03:27";
 
 $done({body: JSON.stringify(obj)});
